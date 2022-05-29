f6b29e8007a60d942bd273fccff3ed4098bec43a
llgava <llgavamt@gmail.com>
2022-05-29 11:41:35 -0400

    Add flipbook_textures.json Schema

---
+++ src/minecraft/MinecraftFlipbookTexture.ts
@@ -0,0 +1,10 @@
+export interface MinecraftFlipbookTexture {
+  atlas_index?: number;
+  atlas_tile_variant?: number;
+  blend_frames?: boolean;
+  replicate?: number;
+  ticks_per_frame?: number;
+  flipbook_texture?: string;
+  atlas_tile?: number;
+  frames?: number[];
+}
---
+++ src/models/ResourcePack/FlipbookTextures.ts
@@ -0,0 +1,4 @@
+import { MinecraftFlipbookTexture } from '@minecraft/MinecraftFlipbookTexture';
+
+/** @$id llgava.resource_packs.FlipbookTextures */
+export type FlipbookTexture = Array<MinecraftFlipbookTexture>;
